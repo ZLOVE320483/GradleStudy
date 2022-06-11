252776054a3f7afd317019bbbc5752b47484b782
zlove.zhang <zlove.zhang@bytedance.com>
2022-06-11 15:49:22 +0800

    add maven-publish & sub-module for test

---
+++ sub-module/src/main/java/com/zlove/gradle/sub/module/SubModuleActivity.kt
@@ -0,0 +1,15 @@
+package com.zlove.gradle.sub.module
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Author by zlove, Email zlove.zhang@bytedance.com, Date on 2022/6/11.
+ */
+class SubModuleActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_module)
+    }
+}
