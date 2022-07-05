1d22bbc7f9734cbddb2995f2e883883cda63cd39
zlove.zhang <zlove.zhang@bytedance.com>
2022-07-05 21:21:42 +0800

    feat: AutoLogTransformTask

--- buildSrc/src/main/kotlin/com/zlove/gradle/plugins/tasks/AutoLogTransformTask.kt
+++ buildSrc/src/main/kotlin/com/zlove/gradle/plugins/tasks/AutoLogTransformTask.kt
@@ -1,9 +1,9 @@
 package com.zlove.gradle.plugins.tasks
 
-import com.android.build.api.transform.QualifiedContent
-import com.android.build.api.transform.Transform
-import com.android.build.api.transform.TransformInvocation
+import com.android.build.api.transform.*
 import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
+import com.zlove.gradle.plugins.utils.SystemPrint
 
 /**
  * Author by zlove, Email zlove.zhang@bytedance.com, Date on 2022/7/4.
@@ -33,5 +33,29 @@ open class AutoLogTransformTask: Transform() {
 
     override fun transform(transformInvocation: TransformInvocation?) {
         super.transform(transformInvocation)
+        transformInvocation?: return
+
+        transformInvocation.referencedInputs
+        val context = transformInvocation.context
+        SystemPrint.outPrintln("project name is ${context.projectName}\n"
+                + "variantName is ${context.variantName}\n"
+                + "path is ${context.path}\n")
+
+    }
+
+    private fun writeOutputFile(input: QualifiedContent,
+                                format: Format,
+                                outputsProvider: TransformOutputProvider) {
+        val outputFile = outputsProvider.getContentLocation(
+            input.name,
+            input.contentTypes,
+            input.scopes,
+            format
+        )
+        if (format == Format.JAR) {
+            FileUtils.copyFile(input.file, outputFile)
+        } else {
+            FileUtils.copyDirectory(input.file, outputFile)
+        }
     }
 }
