16ba0b99cc5f2d4ff645f1c04f9e77775e951cb3
zlove.zhang <zlove.zhang@bytedance.com>
2022-07-13 20:25:00 +0800

    Test AutoLog

--- app/src/main/java/com/zlove/gradle/study/MainActivity.kt
+++ app/src/main/java/com/zlove/gradle/study/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zlove.gradle.study
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,8 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showToast()
     }
 
-
+    private fun showToast() {
+        Toast.makeText(this, "Test Toast", Toast.LENGTH_SHORT).show()
+    }
 
 }
